8441d3bb8be6568cda0c0c5e494142d7fe339bfe
chuxue <chuxuehaocai@proton.me>
2026-09-19 22:13:59 +0800

    Configure Maven Central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 plugins {
     kotlin("jvm") version "2.3.21"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 val ktorVersion = "3.3.2"
 val fastjsonVersion = "2.0.60"
 
 group = "dev.naominet"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -25,6 +28,64 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("Purple Framework")
+                description.set("A Kotlin/JVM bot framework for OneBot-style message JSON.")
+                url.set("https://github.com/chuxuehaocai/purple-framework")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("chuxuehaocai")
+                        name.set("chuxue")
+                        email.set("chuxuehaocai@proton.me")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/chuxuehaocai/purple-framework.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/chuxuehaocai/purple-framework.git")
+                    url.set("https://github.com/chuxuehaocai/purple-framework")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = providers.gradleProperty("signingKey").orNull
+    val signingPassword = providers.gradleProperty("signingPassword").orNull
+    if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    sign(publishing.publications)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
+    }
 }
 
 tasks.test {
